555a661ab3c32b417457d6ebf2e70834f9cc96fd
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2013-01-06 20:22:11 -0500

    Added author's name and made relationship with the user model in the app.

--- app/models/blorgh/post.rb
+++ app/models/blorgh/post.rb
@@ -1,6 +1,15 @@
 module Blorgh
   class Post < ActiveRecord::Base
-    attr_accessible :text, :title
+    attr_accessible :text, :title, :author_name
+    belongs_to :author, class_name: "User"
     has_many :comments
+
+    before_save :set_author
+
+    private
+      def set_author
+        self.author = User.find_or_create_by_name(author_name)
+      end
+    
   end
 end
